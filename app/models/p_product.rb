1ca1a30dc46c4f5601392471dc5142efcf85ed59
7920gh <150332630+7920gh@users.noreply.github.com>
2024-01-10 11:12:31 +0000

    uniqueness

--- app/models/p_product.rb
+++ app/models/p_product.rb
@@ -2,5 +2,6 @@ class PProduct < ApplicationRecord
 
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to:0.01}
+  validates :title, uniqueness: true
 
 end
