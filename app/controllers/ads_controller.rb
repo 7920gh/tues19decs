6da571788e6e6bd616434020d45bf5de9abfd118
7920gh <150332630+7920gh@users.noreply.github.com>
2024-01-22 19:47:11 +0000

    ads

---
+++ app/controllers/ads_controller.rb
@@ -0,0 +1,14 @@
+class AdsController < ApplicationController
+
+
+
+
+
+
+
+
+
+
+
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,43 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# require 'faker'
+
+
+
+
+
+
+
+
+
+#ads.destroy_all
+
+5.times do |index|
+Ad.create!([{
+name: "frank #{index}",
+description: "nice bike #{index}",
+price: "#{index}"+"5",
+seller_id: "#{index}"+"1",
+email: "frank@weymouth"+ "#{index}",
+img_url: "https://www.cyrusher.co.uk/products/xf800-ebike?variant=42606516142293&currency=GBP&utm_medium=product_sync&utm_source=google&utm_content=sag_organic&utm_campaign=sag_organic&https://www.cyrusher.co.uk/?utm_source=google&utm_medium=roas&utm_campaign=Pmax2&utm_id=Pmax2&gad_source=1&gclid=Cj0KCQiAwbitBhDIARIsABfFYILAUnvBht5HYqQT-qNTes7BibxAxaomNMBvUlfB7qYSd2HvIv_gkq8aAuTTEALw_wcB"
+
+  
+}])
+
+end
+
+
+
+
+ # ads.create!(name: "Name #{index}",
+  #              description: "Description #{index}",
+   #             price: "Price #{index}",
+    #            seller_id: "Seller_id #{index}",
+     #           email: "Email #{index}",
+      #          image_url: "Image_url #{index}")				
+				
+				
+#end
+
+#p "Created #{ads.count} ads"
