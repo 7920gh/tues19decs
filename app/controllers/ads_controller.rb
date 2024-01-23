79699c5728b4f0bf8d12fdacf134205de8ac5a1d
7920gh <150332630+7920gh@users.noreply.github.com>
2024-01-23 10:32:42 +0000

    ads

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -9,7 +9,10 @@ end
 
 
 
-
+ # Only allow a list of trusted parameters through.
+    def ad_params
+      params.require(:ticket).permit(:name, :description, :price, :seller_id, :email, :img_url)
+    end
 
 
 
