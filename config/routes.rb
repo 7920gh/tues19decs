180d063d764d6fefc847fb2f53322b00c5fa4fc8
7920gh <150332630+7920gh@users.noreply.github.com>
2024-01-29 13:02:35 +0000

    ads

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
 #get '/ads/',:controller=>'ads',:action=>'show'
 #get 'ads/show'
 get 'adds', :controller=>'adds', :action=>'show'
-#get '/adds/:id', :controller=>'adds', :action=>'show'
+get 'adds/:id', :controller=>'adds', :action=>'show'
 
 
 #get "adds", as: "something#viral" => "adds"
