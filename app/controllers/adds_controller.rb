06c0642601f85ed6df09c55cc1941aedb0eea78d
7920gh <150332630+7920gh@users.noreply.github.com>
2024-01-30 21:11:43 +0000

    ads

--- app/controllers/adds_controller.rb
+++ app/controllers/adds_controller.rb
@@ -11,11 +11,11 @@ end
 
 
 def index
-@add = Add.all
+#@add = Add.all
 #@add = Add.find_by(id: params[:id])
 #@products = Product.all
-
-
+@products=Product.order(:title)
+@adds=Add.order(:name)
 
 
 end
