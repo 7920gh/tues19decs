46c0cf44dca88ef0ab3ed106bb421614f202c5e9
7920gh <150332630+7920gh@users.noreply.github.com>
2024-01-30 16:25:14 +0000

    ads

--- test/controllers/adds_controller_test.rb
+++ test/controllers/adds_controller_test.rb
@@ -4,4 +4,10 @@ class AddsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  
+   test "should get show" do
+   get adds_show_url
+   assert_response :success
+   assert_select "title", "Home | Ruby"
+  end
 end
--- test/controllers/ads_controller_test.rb
+++ test/controllers/ads_controller_test.rb
@@ -4,4 +4,7 @@ class AdsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  
+ 
+  
 end
