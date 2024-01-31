2fcfc5861312555c3c4fe4fe2ef8b536c34d364e
7920gh <150332630+7920gh@users.noreply.github.com>
2024-01-31 12:25:37 +0000

    ads

--- app/controllers/adds_controller.rb
+++ app/controllers/adds_controller.rb
@@ -1,6 +1,6 @@
 class AddsController < ApplicationController
 #before_action :set_product, only: %i[ show ]
-before_action :set_product, only: %i[ show edit update destroy ]
+#before_action :set_product, only: %i[ show edit update destroy ]
 
 def show
 
@@ -36,16 +36,7 @@ end
 
 
 
- private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @add = Add.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def product_params
-      params.require(:add).permit(:name, :description, :price, seller_id, :email, :image_url)
-    end
+ 
 
 
 
