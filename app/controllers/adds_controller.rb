9d6604d50be035fce30893f81ea58ebb21386506
7920gh <150332630+7920gh@users.noreply.github.com>
2024-01-31 20:42:02 +0000

    ads

--- app/controllers/adds_controller.rb
+++ app/controllers/adds_controller.rb
@@ -5,7 +5,7 @@ class AddsController < ApplicationController
 def show
 
 #@adds = Add.find(id:)<% @adds.each do |add| %><p><%= add.name %></p><% end %>
-
+#@add=Add.find(params[:id])
 # products = Add.find_by(id: params[:id])
 @products = Product.all
 end
@@ -19,7 +19,7 @@ def index
 #@products=Product.order(:title)
 @adds=Add.order(:name)
 @products = Product.all
-
+#@adds=Add.find(:all)
 
 end
 def new
